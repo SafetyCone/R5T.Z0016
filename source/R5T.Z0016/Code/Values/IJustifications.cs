13355b9a6fc4c0e475261509681959ccf753e924
davidcoats <david@getsafetycone.com>
2022-10-26 04:20:11 -0700

    Interim changes.

--- source/R5T.Z0016/Code/Values/IJustifications.cs
+++ source/R5T.Z0016/Code/Values/IJustifications.cs
@@ -9,8 +9,10 @@ namespace R5T.Z0016
 	public partial interface IJustifications : IValuesMarker
 	{
 		public const string ExtensionsCanBeWithoutNamespaces_Constant = "Extension methods can be in the global namespace.";
+		public const string LoggingTemplatesCanBeDynamic_Constant = "Whatever. Logging string templates *can* vary between logging calls.";
 
 
 		public string ExtensionsCanBeWithoutNamespaces => IJustifications.ExtensionsCanBeWithoutNamespaces_Constant;
+		public string LoggingTemplatesCanBeDynamic => IJustifications.LoggingTemplatesCanBeDynamic_Constant;
 	}
 }
